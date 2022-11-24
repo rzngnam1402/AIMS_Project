7a2b0dc1b983fa35e607126ccba79501c21c11cb
rzngnam1402 <legiangnam1402@gmail.com>
2022-11-24 14:08:41 +0700

    bugfix

--- Week3/Cart.java
+++ Week3/Cart.java
@@ -1,42 +1,42 @@
 public class Cart {
-   public static final int MAX_NUMBERS_ORDERED = 20;
-   private DigitalVideoDisc itemOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
-   private int qtyOrdered = 0;
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private final DigitalVideoDisc[] itemOrdered = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+    private int qtyOrdered = 0;
 
-   void addDigitalVideoDisc(DigitalVideoDisc disc) {
-      if (qtyOrdered == MAX_NUMBERS_ORDERED) {
-         System.out.println("The cart is already full");
-      } else {
-         this.itemOrdered[qtyOrdered] = disc;
-         qtyOrdered++;
-         System.out.println("The disc has been added");
-      }
-   }
+    void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered == MAX_NUMBERS_ORDERED) {
+            System.out.println("The cart is already full");
+        } else {
+            this.itemOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        }
+    }
 
-   public int getQtyOrdered() {
-      return qtyOrdered;
-   }
+    public int getQtyOrdered() {
+        return qtyOrdered;
+    }
 
-   void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-      if (qtyOrdered == 0) {
-         System.out.println("The cart is null");
-      } else {
-         for (int i = 0; i < qtyOrdered; i++) {
-            if (this.itemOrdered[i] == disc) {
-               this.itemOrdered[i] = this.itemOrdered[qtyOrdered - 1];
-               this.itemOrdered[qtyOrdered - 1] = null;
-               qtyOrdered--;
+    void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (qtyOrdered == 0) {
+            System.out.println("The cart is null");
+        } else {
+            for (int i = 0; i < qtyOrdered; i++) {
+                if (this.itemOrdered[i] == disc) {
+                    this.itemOrdered[i] = this.itemOrdered[qtyOrdered - 1];
+                    this.itemOrdered[qtyOrdered - 1] = null;
+                    qtyOrdered--;
+                }
             }
-         }
-         System.out.println("The disc has been deleted");
-      }
-   }
+            System.out.println("The disc has been deleted");
+        }
+    }
 
-   float totalCost() {
-      float sum = 0;
-      for (int i = 0; i < qtyOrdered; i++) {
-         sum += this.itemOrdered[i].getCost();
-      }
-      return sum;
-   }
+    float totalCost() {
+        float sum = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            sum += this.itemOrdered[i].getCost();
+        }
+        return sum;
+    }
 }
