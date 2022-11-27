e26599d1b31fddd3288729dd39adc9e4284fb09e
rzngnam1402 <legiangnam1402@gmail.com>
2022-11-27 17:47:09 +0700

    topic/store

---
+++ Week3/Store.java
@@ -0,0 +1,34 @@
+public class Store {
+    public static final int STORE_LIMITS = 50;
+    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[STORE_LIMITS];
+    private int qtyStore = 0;
+
+    public int getQtyOrdered() {
+        return qtyStore;
+    }
+
+    public void addDVD(DigitalVideoDisc disc) {
+        if (qtyStore == STORE_LIMITS) {
+            System.out.println("The store is already full");
+        } else {
+            this.itemsInStore[qtyStore] = disc;
+            qtyStore++;
+            System.out.println("The disc has been added to store");
+        }
+    }
+
+    public void removeDVD(DigitalVideoDisc disc) {
+        if (qtyStore == 0) {
+            System.out.println("The store is null");
+        } else {
+            for (int i = 0; i < qtyStore; i++) {
+                if (this.itemsInStore[i] == disc) {
+                    this.itemsInStore[i] = this.itemsInStore[qtyStore - 1];
+                    this.itemsInStore[qtyStore - 1] = null;
+                    qtyStore--;
+                }
+            }
+            System.out.println("The disc has been deleted from store");
+        }
+    }
+}
